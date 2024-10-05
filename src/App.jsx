8181a0a0b113bb3bd7530052ec6e19b439c430ee
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-10-05 19:26:13 +0200

    finished HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 import SearchBox from "./components/SearchBox";
@@ -13,24 +13,14 @@ const initialContacts = [
 ];
 
 function App() {
-  const [contacts, setContacts] = useState(initialContacts);
-//1)  ______________________________________________________________________________
-  // const savedContacts = window.localStorage.getItem("saved-contacts");
-  // const [contacts, setContacts] = useState(savedContacts ? JSON.parse(savedContacts) : [
-  // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-// ]);  
-//2)  ______________________________________________________________________________
-  // const [contacts, setContacts] = useState(() => {
-  //   const savedContacts = window.localStorage.getItem("saved-contacts");
-  //   return savedContacts ? JSON.parse(savedContacts) : {initialContacts};
-  // });
+  const [contacts, setContacts] = useState(() => {
+  const savedContacts = window.localStorage.getItem("saved-contacts");
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
 
-  // useEffect(() => {
-  //   window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
-  // }, [contacts]);
+  useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
+  }, [contacts]);
 // ______________________________________________________________________________
   const [contactFilter, setContactFilter] = useState("");
 
